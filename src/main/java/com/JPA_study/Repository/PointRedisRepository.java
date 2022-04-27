0e8832eebb4eb25034fa083692584473d6b0e273
KIM-JS-95 <baugh248730@gmail.com>
2022-04-27 22:11:59 +0900

    123

--- src/main/java/com/JPA_study/Repository/PointRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.JPA_study.Repository;
-
-import com.JPA_study.entity.Point;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PointRedisRepository extends CrudRepository<Point, String> {
-}
